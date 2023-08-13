f2db6729d20f10c70069d0a27db1da3cf0b67c2e
Xz3ra <mathieu_durand@icloud.com>
2023-08-12 20:41:56 -0400

    Removed commented out code

--- src/main/java/net/xz3ra/www/karaokeplayer/PlayerController.java
+++ src/main/java/net/xz3ra/www/karaokeplayer/PlayerController.java
@@ -1,29 +1,16 @@
 package net.xz3ra.www.karaokeplayer;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import net.xz3ra.www.karaokeplayer.exceptions.ExceptionAlertHandler;
-import net.xz3ra.www.karaokeplayer.exceptions.MissingFilesException;
-import net.xz3ra.www.karaokeplayer.exceptions.UnsupportedFileTypeException;
 import net.xz3ra.www.karaokeplayer.karaoke.Karaoke;
 import net.xz3ra.www.karaokeplayer.karaoke.KaraokePlayer;
 import net.xz3ra.www.karaokeplayer.karaoke.KaraokeView;
 import net.xz3ra.www.karaokeplayer.media.MediaPlayerControl;
 
 public class PlayerController {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private KaraokeView karaokeView;
@@ -73,7 +60,6 @@ public class PlayerController {
                 playerControl.setDisable(false);
             }
         } catch (Exception e) {
-            //throw new RuntimeException(e);
             ExceptionAlertHandler.showAlert(e);
         } finally {
             if (oldKaraokePlayer != null) {
@@ -84,7 +70,7 @@ public class PlayerController {
 
     @FXML
     void initialize() {
-        playerControl.setEventRoot(karaokeView.getParent());
+        playerControl.setEventRoot(getRoot());
         playerControl.fadedProperty().addListener((observable, oldFadedValue, newFadedValue) -> {
             if (newFadedValue != null && scene != null) {
                 scene.setCursor(newFadedValue ? Cursor.NONE : Cursor.DEFAULT);
